c9b91c81a2dd8131e664e3f018005f82360ed05d
Hasnat <52853253+thehasnism@users.noreply.github.com>
2025-04-27 11:53:37 +0500

    changes

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import os
-import sys
 import tkinter as tk
 from tkinter import filedialog
 import threading
@@ -8,19 +7,22 @@ import shutil
 
 def delFolder(base_path, relative_path):
     full_path = os.path.join(base_path, relative_path)
-    try:
-        shutil.rmtree(full_path)
-        print(f"Deleted: {full_path}")
-    except FileNotFoundError:
-        print(f"File not found: {full_path}")
-    except Exception as e:
-        print(f"Error deleting {full_path}: {e}")
+
+    if os.path.exists(full_path):  # Only proceed if the folder exists
+        try:
+            shutil.rmtree(full_path)
+            print(f"Deleted: {full_path}")
+        except Exception as e:
+            print(f"Error deleting {full_path}: {e}")
+    else:
+        print(f"Folder not found: {full_path}")
 
 
 def delete_folders_in_projects(root_path):
     # List of relative paths to delete
     paths_to_delete = [
         "app/build",
+        "main/build",
         ".gradle",
         ".idea",
         "app/release"
@@ -29,8 +31,11 @@ def delete_folders_in_projects(root_path):
     # Iterate through all directories within the root_path
     for dirpath, dirnames, filenames in os.walk(root_path):
         for path in paths_to_delete:
-            thread = threading.Thread(target=delFolder, args=(dirpath, path))
-            thread.start()
+            # Check if path is within the current directory
+            full_path = os.path.join(dirpath, path)
+            if os.path.exists(full_path):  # Only start a thread if the folder exists
+                thread = threading.Thread(target=delFolder, args=(dirpath, path))
+                thread.start()
 
 
 if __name__ == '__main__':
